64b404549e4c42a382c8b9dae21f99dc1542e571
BuRnCycL <burncycl@users.noreply.github.com>
2021-01-19 14:58:25 -0700

    Update init.sh
    Added variables for venv dir.

--- init.sh
+++ init.sh
@@ -9,11 +9,11 @@ VENV_DIR="./venv"
 
 if [ -d "$VENV_DIR" ]; then
 	echo "Virtual Environment directory exists. Activating..." 
-	source ./venv/bin/activate
+	source ${VENV_DIR}/bin/activate
 else
 	echo "Virtual Environment directory does not exist. Creating and Initializing..." 
-	virtualenv -p python3 ./venv
-	source ./venv/bin/activate
+	virtualenv -p python3 ${VENV_DIR}
+	source ${VENV_DIR}/bin/activate
 	pip3 install -r ./requirements.txt
 	# Use `deactivate` to exit Python3 virtual environment.
 fi
